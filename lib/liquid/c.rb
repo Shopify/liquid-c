e6afac29062dfb33510b7eeac5ccbdbf669ad9dd
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2014-03-28 22:26:32 -0400

    Workaround bundler issue which could cause an old extension to get used.
    See https://github.com/bundler/bundler/issues/2947

--- lib/liquid/c.rb
+++ lib/liquid/c.rb
@@ -1,6 +1,14 @@
 require 'liquid/c/version'
 require 'liquid'
-require 'liquid_c'
+
+# workaround bundler issue #2947 which could cause an old extension to be used
+require 'rbconfig'
+ext_path = File.expand_path("../../liquid_c.#{RbConfig::CONFIG['DLEXT']}", __FILE__)
+if File.exists?(ext_path)
+  require ext_path
+else
+  require 'liquid_c'
+end
 
 Liquid::Template.class_eval do
   private

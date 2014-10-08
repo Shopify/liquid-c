c8c94a1a0fd1320a933cf2351fe2fe9b8ee751da
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2014-10-08 16:36:04 -0400

    Revert "Remove C variable parsing (revert this later)"
    This reverts commit 184e9ab2629bf51b72def993d1b262dd28bd37c9.

--- ext/liquid_c/liquid.c
+++ ext/liquid_c/liquid.c
@@ -1,5 +1,6 @@
 #include "liquid.h"
 #include "tokenizer.h"
+#include "variable.h"
 
 VALUE mLiquid;
 rb_encoding *utf8_encoding;
@@ -9,4 +10,5 @@ void Init_liquid_c(void)
     utf8_encoding = rb_utf8_encoding();
     mLiquid = rb_define_module("Liquid");
     init_liquid_tokenizer();
+    init_liquid_variable();
 }

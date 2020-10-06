f683ac8f9a7982905222bbf1c0b3ee5021da9b00
Dylan Thacker-Smith <dylan.smith@shopify.com>
2020-10-06 13:28:53 -0400

    Add liquid_vm_next_instruction to reduce duplication (#72)

--- ext/liquid_c/vm.h
+++ ext/liquid_c/vm.h
@@ -6,5 +6,6 @@
 
 void init_liquid_vm();
 void liquid_vm_render(block_body_t *block, VALUE context, VALUE output);
+void liquid_vm_next_instruction(const uint8_t **ip_ptr, const size_t **const_ptr_ptr);
 
 #endif

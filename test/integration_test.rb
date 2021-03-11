7e2814c9088613546c5b0ad9a1b98962a97e6f21
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2021-03-11 14:08:52 -0800

    Provide test/liquid_test_helper to make it easier to run specific liquid tests
    from the liquid repo's test suite.

--- test/integration_test.rb
+++ test/integration_test.rb
@@ -1,18 +1,9 @@
 # frozen_string_literal: true
-liquid_lib_dir = $LOAD_PATH.detect { |p| File.exist?(File.join(p, 'liquid.rb')) }
-liquid_test_dir = File.join(File.dirname(liquid_lib_dir), 'test')
-$LOAD_PATH << liquid_test_dir
 
-require 'test_helper'
-require 'liquid/c'
+require_relative 'liquid_test_helper'
 
-if ENV['LIQUID_C_DISABLE_VM']
-  puts "-- Liquid-C VM Disabled"
-  Liquid::ParseContext.liquid_c_nodes_disabled = true
-end
-
-test_files = Dir[File.join(liquid_test_dir, 'integration/**/*_test.rb')]
-test_files << File.join(liquid_test_dir, 'unit/tokenizer_unit_test.rb')
+test_files = Dir[File.join(LIQUID_TEST_DIR, 'integration/**/*_test.rb')]
+test_files << File.join(LIQUID_TEST_DIR, 'unit/tokenizer_unit_test.rb')
 test_files.each do |test_file|
   require test_file
 end

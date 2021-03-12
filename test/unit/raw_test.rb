a92424d641515f4f33e8fab319b8309cc55e7ff8
Dylan Thacker-Smith <dylan.smith@shopify.com>
2021-03-12 12:57:01 -0500

    Fix support for derived classes of Liquid::Raw (#148)

---
+++ test/unit/raw_test.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class RawTest < Minitest::Test
+  class RawWrapper < Liquid::Raw
+    def render_to_output_buffer(_context, output)
+      output << "<"
+      super
+      output << ">"
+    end
+  end
+  Liquid::Template.register_tag("raw_wrapper", RawWrapper)
+
+  def test_derived_class
+    output = Liquid::Template.parse("{% raw_wrapper %}body{% endraw_wrapper %}").render!
+    assert_equal("<body>", output)
+  end
+end

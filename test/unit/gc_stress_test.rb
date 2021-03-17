c2c75bf88a305f08f3b2740d561db25f06bcb717
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2021-03-16 17:35:16 -0700

    Add a GC stress test as a regression test for the bug fixed by #147

---
+++ test/unit/gc_stress_test.rb
@@ -0,0 +1,24 @@
+# encoding: utf-8
+# frozen_string_literal: true
+require 'test_helper'
+
+# Help catch bugs from objects not being marked at all
+# GC opportunities.
+class GCStressTest < Minitest::Test
+  def test_compile_and_render
+    source = '{% assign x = 1 %}{% if x -%} x: {{ x | plus: 2 }}{% endif %}'
+    result = gc_stress do
+      Liquid::Template.parse(source).render!
+    end
+    assert_equal("x: 3", result)
+  end
+
+  private
+
+  def gc_stress
+    GC.stress = true
+    yield
+  ensure
+    GC.stress = false
+  end
+end

fb6be427d7f8d75a94735a5a63318a0a00fa4b52
Michael Go <ggmichaelgo@gmail.com>
2022-02-04 21:13:53 +0000

    add more split filter test

--- test/unit/filter_test.rb
+++ test/unit/filter_test.rb
@@ -6,6 +6,22 @@ class FilterTest < MiniTest::Test
   def test_filter_split
     source = "{{ 'ooooooooox' | split: 'o' }}"
     template = Liquid::Template.parse(source)
-    puts template.render
+    assert_equal "x", template.render
+  end
+
+  def test_filter_split_missing_argument_error
+    source = "{{ 'ooooooooox' | split }}"
+    template = Liquid::Template.parse(source)
+
+    exc = assert_raises(Liquid::ArgumentError) { template.render }
+    assert_equal("Liquid error: wrong number of arguments (given 0, expected 1)", exc.message)
+  end
+
+  def test_filter_split_wrong_arguement_error
+    source = "{{ 'ooooooooox' | split: 'o' foo: '1'}}"
+    template = Liquid::Template.parse(source)
+
+    exc = assert_raises(Liquid::ArgumentError) { template.render }
+    assert_equal("Liquid error: wrong number of arguments (given 2, expected 1)", exc.message)
   end
 end

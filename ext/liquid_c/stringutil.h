9b2d5d415a99a0f1e012e13a3239c56e6f06f096
Franklyn Dsouza <franklyn.dsouza@shopify.com>
2022-02-23 15:53:23 -0500

    identifier matcher

--- ext/liquid_c/stringutil.h
+++ ext/liquid_c/stringutil.h
@@ -3,7 +3,39 @@
 #if !defined(LIQUID_UTIL_H)
 #define LIQUID_UTIL_H
 
-char* vcompare2(char* cursor, char needle) {
+/*
+inline static int is_identifier(char c)
+{
+    return ISALNUM(c) || c == '_' || c == '-';
+}
+while (++cur < end && is_identifier(*cur)) {}
+*/
+inline static const char* isidentifier(char* cursor) {
+    const __m256i upperA = _mm256_set1_epi8('A' - 1);
+    const __m256i upperZ = _mm256_set1_epi8('Z' + 1);
+    const __m256i lowerA = _mm256_set1_epi8('a' - 1);
+    const __m256i lowerZ = _mm256_set1_epi8('z' + 1);
+    const __m256i digit0 = _mm256_set1_epi8('0' - 1);
+    const __m256i digit9 = _mm256_set1_epi8('9' + 1);
+    const __m256i haystack = _mm256_loadu_si256((const __m256i*)cursor);
+
+    const __m256i geupperA = _mm256_cmpgt_epi8(haystack, upperA);
+    const __m256i leupperZ = _mm256_cmpgt_epi8(upperZ, haystack);
+    const __m256i gelowerA = _mm256_cmpgt_epi8(haystack, lowerA);
+    const __m256i lelowerZ = _mm256_cmpgt_epi8(lowerZ, haystack);
+    const __m256i gedigit0 = _mm256_cmpgt_epi8(haystack, digit0);
+    const __m256i ledigit9= _mm256_cmpgt_epi8(digit9, haystack);
+
+    int result = _mm256_movemask_epi8(geupperA & leupperZ);
+    result |= _mm256_movemask_epi8(gelowerA & lelowerZ);
+    result |= _mm256_movemask_epi8(gedigit0 & ledigit9);
+
+    result = ~result;
+
+    return cursor + __builtin_ctz(result);
+}
+
+inline static const char* vcompare2(char* cursor, char needle) {
     const __m256i haystack = _mm256_loadu_si256((const __m256i*)cursor);
     const __m256i needles = _mm256_set1_epi8(needle);
     const __m256i eq = _mm256_cmpeq_epi8(haystack, needles);
@@ -12,7 +44,7 @@ char* vcompare2(char* cursor, char needle) {
     return cursor + __builtin_ctz(result);
 }
 
-char* block_search(char* cursor, char* last, char needle) {
+inline static const char* block_search(char* cursor, char* last, char needle) {
     char* test;
     while((last - cursor + 1) >= 32) {
         test = vcompare2(cursor, needle);

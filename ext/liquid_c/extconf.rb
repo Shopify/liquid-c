a46872ed72dbf9246d9ac95bf2713fc26b01f9a9
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2022-04-20 17:23:10 -0400

    Remove error on compiler warnings by default, except on CI
    Backport of commit c86424c6bac15367d8b05d408c31a89e95018f6e

--- ext/liquid_c/extconf.rb
+++ ext/liquid_c/extconf.rb
@@ -1,5 +1,11 @@
 require 'mkmf'
-$CFLAGS << ' -Wall -Werror -Wextra -Wno-unused-parameter -Wno-missing-field-initializers'
+$CFLAGS << ' -std=c99 -Wall -Wextra -Wno-unused-parameter -Wno-missing-field-initializers'
+# In Ruby 2.6 and earlier, the Ruby headers did not have struct timespec defined
+valid_headers = RbConfig::CONFIG['host_os'] !~ /linux/ || Gem::Version.new(RUBY_VERSION) >= Gem::Version.new("2.7")
+pedantic = !ENV['LIQUID_C_PEDANTIC'].to_s.empty?
+if pedantic && valid_headers
+  $CFLAGS << ' -Werror'
+end
 compiler = RbConfig::MAKEFILE_CONFIG['CC']
 if ENV['DEBUG'] == 'true' && compiler =~ /gcc|g\+\+/
   $CFLAGS << ' -fbounds-check'
